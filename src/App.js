1eccc752cce427f773e6bee02b9d8b00c4796cf9
KorNito <kornelijuspacevicius@gmail.com>
2020-11-17 20:48:49 +0200

    Added navigation to home, ,rooms, single rooms and error pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import { Route, Switch, Link} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/rooms/' component={Rooms}></Route>
+        <Route exact path='/rooms/:id' component={SingleRoom}></Route>
+        <Route component={Error}></Route>
+      </Switch>
     </>
   );
 }
